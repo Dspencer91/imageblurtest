bf67f5cd1c58d020602c8b3bf943b413cddad09e
dillon spencer <dillonstreetfrontcommercial@gmail.com>
2020-01-30 17:13:13 +0000

    Initial commit

---
+++ lib/image.rb
@@ -0,0 +1,17 @@
+class Image
+
+  def initialize (picture)
+    @picture = picture
+  end
+
+  def get_ones
+    ones = []
+    @picture.each_with_index do |row, row_number|
+      row.each_with_index do |item, col_number|
+        if item == 1
+          ones << [row_number, col_number]
+        end
+      end
+    end
+    ones
+  end
